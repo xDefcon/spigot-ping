2adacb1435e89cb49855391ba37a77cecdfef07e
xDefcon <xdefconhd@gmail.com>
2017-03-28 12:04:07 +0200

    v2.4 - Add ping <target> command.

--- src/com/xdefcon/spigotping/commands/PingCommand.java
+++ src/com/xdefcon/spigotping/commands/PingCommand.java
@@ -3,11 +3,13 @@ package com.xdefcon.spigotping.commands;
 import com.xdefcon.spigotping.SpigotPing;
 import com.xdefcon.spigotping.utils.PingUtil;
 import com.xdefcon.spigotping.utils.SoundUtil;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
 
 public class PingCommand implements CommandExecutor {
     private SpigotPing plugin;
@@ -24,20 +26,41 @@ public class PingCommand implements CommandExecutor {
         }
 
         final Player p = (Player) sender;
-        if (plugin.getConfig().getBoolean("permission-system.enabled")) {
-            if (!p.hasPermission("spigotping.ping")) {
+        if (args.length == 0) {
+            if (!hasPerms(p, "spigotping.ping")) {
                 String noPerm = plugin.getConfig().getString("permission-system.no-perm-message");
                 p.sendMessage(ChatColor.translateAlternateColorCodes('&', noPerm));
                 return true;
             }
+            String ping = "" + PingUtil.getPing(p);
+            String customMex = plugin.getConfig().getString("ping-command.ping-message").replaceAll("%ping%", ping);
+            p.sendMessage(ChatColor.translateAlternateColorCodes('&', customMex));
+        } else {
+            if (!hasPerms(p, "spigotping.ping.others")) {
+                String noPerm = plugin.getConfig().getString("others-ping.not-allowed-message");
+                p.sendMessage(ChatColor.translateAlternateColorCodes('&', noPerm));
+                return true;
+            }
+            String target = args.length > 0 ? args[0] : null;
+            Player targetP = Bukkit.getPlayer(target);
+            if (targetP == null) {
+                p.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                        plugin.getConfig().getString("others-ping.player-not-found")));
+                return true;
+            }
+            p.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                    plugin.getConfig().getString("ping-command.ping-target-message")
+                            .replace("%ping%", "" + PingUtil.getPing(targetP))
+                            .replace("%target%", targetP.getName())));
         }
-        String ping = "" + PingUtil.getPing(p);
-        String customMex = plugin.getConfig().getString("ping-command.ping-message").replaceAll("%ping%", ping);
-        p.sendMessage(ChatColor.translateAlternateColorCodes('&', customMex));
         if (plugin.getConfig().getBoolean("sound-manager.enabled")) {
             SoundUtil.playSound(p, plugin.getConfig().getString("sound-manager.sound-type"));
         }
         return true;
     }
 
+    private boolean hasPerms(Player p, String perm) {
+        return !plugin.getConfig().getBoolean("permission-system.enabled") || p.hasPermission(perm);
+    }
 }
+
