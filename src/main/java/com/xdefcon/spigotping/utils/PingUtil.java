b9a8bb3b646e5ac3d27cebc7b04edd244cf71eca
ProDesert22 <pedroantonioppms@gmail.com>
2021-06-11 19:42:24 -0400

    Change Ping, Config Sound and Maven 1.17

--- src/main/java/com/xdefcon/spigotping/utils/PingUtil.java
+++ src/main/java/com/xdefcon/spigotping/utils/PingUtil.java
@@ -13,12 +13,8 @@ public class PingUtil {
             p = Bukkit.getPlayer(p.getUniqueId()); //cast to org.bukkit.entity.Player
         }
         try {
-            Class<?> CraftPlayerClass = Class.forName("org.bukkit.craftbukkit." + v + ".entity.CraftPlayer");
-            Object CraftPlayer = CraftPlayerClass.cast((Player) p);
-            Method getHandle = CraftPlayer.getClass().getMethod("getHandle");
-            Object EntityPlayer = getHandle.invoke(CraftPlayer);
-            Field ping = EntityPlayer.getClass().getDeclaredField("ping");
-            return ping.getInt(EntityPlayer);
+            int ping = p.getPing();
+            return ping;
         } catch (Exception e) {
             e.printStackTrace();
         }
